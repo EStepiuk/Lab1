8da780434ba2b2550d474e816fa39593a486ccc5
Kryvonis <kryvonis.artem@gmail.com>
2016-03-20 23:01:53 +0200

    add MVC CRUD operation

--- src/model.py
+++ src/model.py
@@ -1,1 +1,31 @@
 __author__ = 'Kryvonis'
+
+
+class MatchResult:
+    def __init__(self, first_T, second_T, result, date):
+        self.date = date
+        self.result = result
+        self.first_T = first_T
+        self.second_T = second_T
+
+    def get_date(self):
+        return self.date
+    def get_result(self, ):
+        return self.result
+
+    def get_first(self):
+        return self.first_T
+
+    def get_second(self):
+        return self.second_T
+
+
+England = [MatchResult('Lester', 'Everton', '2:0','03.18.16'),
+           MatchResult('MU', 'Everton', '3:0','03.16.16'),
+           MatchResult('Chelsea', 'Arsenal', '2:1','03.18.16'),
+           MatchResult('MU', 'Chelsea', '0:0','03.20.16'),
+           MatchResult('Arsenal', 'Lester', '1:1','03.22.16'),
+           MatchResult('Arsenal', 'Chelsea', '3:0','03.25.16')]
+
+
+
