8121a66c265282cb8608f7835e02651c5ebcf015
Yevhen Stepiuk <stepyuk.android@gmail.com>
2016-03-23 15:04:02 +0200

    Documented

--- src/controll.py
+++ src/controll.py
@@ -1,4 +1,4 @@
-__author__ = 'dnt'
+__author__ = 'Stepiuk'
 from os import system
 import model
 import view
@@ -6,13 +6,26 @@ import getch
 
 
 def main_loop():
+    """
+    Main lifecycle
+    :return:
+    """
+    system("clear")
     for t in model.England:
         view.render_item(t.get_first(), t.get_second(), t.get_result(), t.get_date())
+    view.render_menu()
     while True:
         user_interaction_handler()
 
 
 def user_interaction_handler():
+    """
+    Handle user actions:
+    e -> exit
+    s -> search by team
+    space -> full table
+    :return:
+    """
     c = getch.getch()
     system("clear")
     if c == ' ':
