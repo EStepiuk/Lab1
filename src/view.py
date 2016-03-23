74f020241881aab66018964dcfc00a9fa0166a32
Meggapixxel <android.vadim@gmail.com>
2016-03-23 15:00:14 +0200

    Rebuild

--- src/view.py
+++ src/view.py
@@ -1,13 +1,14 @@
 __author__ = 'Zhydenko'
 
 def render_item(team1, team2, score, date):
+    print("\n"+date)
     print(team1 + " vs " + team2 + "  " + score + "  " + date)
     return
 
 def render_menu():
     print ("""
-            1.Space to view all matches
-            2.E to exit
-            3.S to search
+            - Space to view all matches
+            - E to exit
+            - S to search
     """)
     return
