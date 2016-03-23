ae535ffd1e4f35e71ce85389f9562df297e91398
Meggapixxel <android.vadim@gmail.com>
2016-03-23 14:47:15 +0200

    Message

--- src/view.py
+++ src/view.py
@@ -1,8 +1,13 @@
-__author__ = 'Kryvonis'
-
+__author__ = 'Zhydenko'
 
 def render_item(team1, team2, score, date):
+    print(team1 + " vs " + team2 + "  " + score + "  " + date)
     return
 
 def render_menu():
+    print ("""
+            1.Space to view all matches
+            2.E to exit
+            3.S to search
+    """)
     return
