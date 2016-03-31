74d63e5fe3f0c3664864d18e838a86646d34031b
Meggapixxel <android.vadim@gmail.com>
2016-03-31 19:48:19 +0300

    json serialize

---
+++ src/sear_json.py
@@ -0,0 +1,10 @@
+import json
+
+def write(obj, filename='../res/json_file.json'):
+    with open(filename, 'wt') as f:
+        json.dump(obj, f)
+
+
+def read(filename='../res/json_file.json'):
+    with open(filename, 'r') as f:
+        return json.load(f)
